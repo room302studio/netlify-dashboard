aa4e02be4c9b6ec7173939a7472dfbef54135f4e
EJ Fox <ejfox@ejfox.com>
2023-11-28 11:33:52 -0500

    feat: pull error deploys to top

--- README.md
+++ README.md
@@ -1,29 +1,18 @@
-# EJ Fox Nuxt 3 Starter
+# Netlify Dashboard
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/981b9e46-6878-4ddb-a716-2713c5f3e412/deploy-status)](https://app.netlify.com/sites/ejfox-nuxt-template/deploys)
-
-## Usage
-`npx room302-template`
-
-Deployed through a small script that handles naming, cloning, and setting up the repo for prototyping. <https://www.npmjs.com/package/room302-template>
-
-## What's different from the standard Nuxt 3 starter?
-- VueUse 🔧 
-- Vueuse motion 🌈 
-- OpenAI plugin 🧠 
-- Pinia store 🏬 
-- Helpers file 🔨 
-- Google fonts 🖋️ 
-
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
 ## Setup
+
+Make sure to get a Netlify personal access token and add it to `.env` under `NETLIFY_TOKEN`
 
 Make sure to install the dependencies:
 
 ```bash
-# yarn
+# yarn install
 yarn install
+
+# run the app
+yarn dev
 ```
 
 ## Development Server
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,59 @@
 <template>
-  <section>
+  <section class="p-4">
     <div class="dashboard">
       <h1>Netlify Build Status Dashboard</h1>
       <div v-if="loading">Loading...</div>
       <div v-else>
-        <ul class="flex flex-wrap">
-          <UCard v-for="site in sortedSites" :key="site.id" class="site-item w-1/2">
-            <h2 class="text-xl my-2">{{ site.name }}</h2>
-            <p><strong>Last Updated:</strong> {{ new Date(site.updated_at).toLocaleString() }}</p>
-            <!-- <p>
-              <img :src="site?.published_deploy?.screenshot_url" />
-            </p> -->
 
-            <!-- Display the latest deploy status -->
-            <div v-if="site.deploys && site.deploys.length">
+        <!-- a list of just the builds with errors -->
+        <h2>Builds with Errors</h2>
+        <div class="flex flex-wrap">
+          <div class="grid grid-cols-4 gap-4">
+            <UCard v-for="site in sitesWithBuildErrors" :key="site.id" class="site-item">
+              <!-- the name of the site -->
+              <h2 class="text-xl my-2">{{ site.name }}</h2>
+              <!-- details on the build error -->
               <p><strong>Latest Deploy Status:</strong> <span :class="{ 'error': site.deploys[0].state !== 'ready' }">{{
                 site.deploys[0].state }}</span></p>
               <p v-if="site.deploys[0].state !== 'ready'">⚠️ Build Issue!</p>
               <p><strong>Deploy Log:</strong> <a :href="site.deploys[0].log_access_attributes.url" target="_blank">View
                   Logs</a></p>
-            </div>
 
-            <h3>{{ site.custom_domain }}</h3>
-            <p>Public: {{ site.build_settings.public_repo }}</p>
-            <p><strong>URL:</strong> <a :href="site.url">{{ site.url }}</a></p>
-            <!-- <p><strong>Admin URL:</strong> <a :href="site.admin_url">{{ site.admin_url }}</a></p> -->
+              <!-- the linked github repo, if available -->
+              <p v-if="site.build_settings.public_repo">
+                <a :href="site.build_settings.repo_url" target="_blank" class="text-xs font-mono">{{
+                  site.build_settings.repo_url }}</a>
+              </p>
+            </UCard>
+          </div>
+        </div>
 
-          </UCard>
+        <UDivider class="my-24" />
+
+        <ul class="flex flex-wrap">
+          <div v-for="site in sortedSites" :key="site.id" class="site-item w-1/2 p-2 flex flex-col align-center">
+            <UCard>
+              <h2 class="text-xl">{{ site.name }}</h2>
+              <p><strong>Last Updated:</strong> {{ new Date(site.updated_at).toLocaleString() }}</p>
+              <!-- <p>
+                <img :src="site?.published_deploy?.screenshot_url" />
+              </p> -->
+
+              <!-- Display the latest deploy status -->
+              <div v-if="site.deploys && site.deploys.length">
+                <p><strong>Latest Deploy Status:</strong> <span :class="{ 'error': site.deploys[0].state !== 'ready' }">{{
+                  site.deploys[0].state }}</span></p>
+                <p v-if="site.deploys[0].state !== 'ready'">⚠️ Build Issue!</p>
+                <p><strong>Deploy Log:</strong> <a :href="site.deploys[0].log_access_attributes.url" target="_blank">View
+                    Logs</a></p>
+              </div>
+
+              <h3>{{ site.custom_domain }}</h3>
+              <p>Public: {{ site.build_settings.public_repo }}</p>
+              <p><strong>URL:</strong> <a :href="site.url" class="text-xs font-mono">{{ site.url }}</a></p>
+              <!-- <p><strong>Admin URL:</strong> <a :href="site.admin_url">{{ site.admin_url }}</a></p> -->
+            </UCard>
+          </div>
         </ul>
       </div>
     </div>
@@ -60,6 +87,14 @@ onMounted(async () => {
 const sortedSites = computed(() => {
   if (!data.value) return [];
   return data.value.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+});
+
+const sitesWithBuildErrors = computed(() => {
+  if (!data.value) return [];
+  const filtered = data.value.filter(site => site.deploys && site.deploys.length && site.deploys[0].state !== 'ready');
+
+  // sort by last updated
+  return filtered.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
 });
 </script>
 
